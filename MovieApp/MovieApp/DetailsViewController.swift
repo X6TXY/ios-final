ca4cddd0e9bef4f934ea66e19d2e5e7e52093971
ushyndali <uldana.shyndali@prowheelxr.com>
2025-12-13 06:37:24 +0500

    gradient fixed

--- MovieApp/MovieApp/DetailsViewController.swift
+++ MovieApp/MovieApp/DetailsViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-
 struct CastMember {
     let name: String
     let image: UIImage
@@ -16,133 +15,91 @@ struct CastMember {
 class DetailsViewController: UIViewController {
     
     let topCast = [
-               CastMember(name: "Actor 1", image: UIImage(named: "actor")!),
-               CastMember(name: "Actor 2", image: UIImage(named: "actor")!),
-               CastMember(name: "Actor 3", image: UIImage(named: "actor")!),
-               CastMember(name: "Actor 4", image: UIImage(named: "actor")!),
-               CastMember(name: "Actor 5", image: UIImage(named: "actor")!),
-               CastMember(name: "Actor 6", image: UIImage(named: "actor")!)
-           ]
+        CastMember(name: "Actor 1", image: UIImage(named: "actor")!),
+        CastMember(name: "Actor 2", image: UIImage(named: "actor")!),
+        CastMember(name: "Actor 3", image: UIImage(named: "actor")!),
+        CastMember(name: "Actor 4", image: UIImage(named: "actor")!),
+        CastMember(name: "Actor 5", image: UIImage(named: "actor")!),
+        CastMember(name: "Actor 6", image: UIImage(named: "actor")!)
+    ]
     
     @IBOutlet weak var expandButton: UIButton!
     @IBOutlet weak var synopsisContent: UILabel!
-    @IBOutlet weak var synopsisGradient: UIView!
-    
+    @IBOutlet weak var synopsisGradient: GradientView!
     @IBOutlet weak var castStackView: UIStackView!
-    
     @IBOutlet weak var movieInfoContainer: UIView!
-    @IBOutlet weak var movieInfoGradient: UIView!
+    @IBOutlet weak var movieInfoGradient: GradientView!
     
     private var isExpanded = false
-    private var gradientLayer: CAGradientLayer?
+    private var synopsisGradientLayer: CAGradientLayer?
     private var movieInfoGradientLayer: CAGradientLayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupNavigationBar()
         setupTabBar()
-
+        
         synopsisContent.numberOfLines = 3
         expandButton.setImage(UIImage(systemName: "arrow.down"), for: .normal)
-
-        // CREATE gradient layers once
-        setupGradient()
-        setupMovieInfoGradient()
-    }
-    
-    
-    private func setupNavigationBar() {
-        if let navBar = navigationController?.navigationBar {
-            navBar.setBackgroundImage(UIImage(), for: .default)
-            navBar.shadowImage = UIImage()
-            navBar.isTranslucent = true
-            navBar.backgroundColor = .clear
-            navBar.tintColor = .white
-        }
-    }
-    
-    private func setupTabBar() {
-        if let tabBar = tabBarController?.tabBar {
-            tabBar.backgroundImage = UIImage()
-            tabBar.shadowImage = UIImage()
-            tabBar.isTranslucent = true
-            tabBar.backgroundColor = .clear
-        }
-    }
-    
-    private func setupGradient() {
-        if gradientLayer == nil {
-            let gradient = CAGradientLayer()
-            gradient.colors = [
-                view.backgroundColor?.withAlphaComponent(0.0).cgColor ?? UIColor.clear.cgColor,
-                view.backgroundColor?.cgColor ?? UIColor.white.cgColor
-            ]
-            gradient.startPoint = CGPoint(x: 0.5, y: 0)
-            gradient.endPoint = CGPoint(x: 0.5, y: 1)
-            gradient.locations = [0.0, 0.3]
-            synopsisGradient.layer.insertSublayer(gradient, at: 0)
-            gradientLayer = gradient
-            synopsisGradient.isUserInteractionEnabled = false
-        }
-    }
-
-    private func setupMovieInfoGradient() {
-        if movieInfoGradientLayer == nil {
-            let gradient = CAGradientLayer()
-            gradient.colors = [
-                view.backgroundColor?.cgColor ?? UIColor.white.cgColor,
-                (view.backgroundColor?.withAlphaComponent(0.0).cgColor) ?? UIColor.clear.cgColor
-            ]
-            gradient.startPoint = CGPoint(x: 0.5, y: 1)
-            gradient.endPoint = CGPoint(x: 0.5, y: 0)
-            gradient.locations = [0.0, 1.0]
-            movieInfoGradient.layer.insertSublayer(gradient, at: 0)
-            movieInfoGradientLayer = gradient
-            movieInfoGradient.isUserInteractionEnabled = false
-        }
-    }
-
-    override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
         
-        // only update frames, do not recreate layers
-        gradientLayer?.frame = synopsisGradient.bounds
-        movieInfoGradientLayer?.frame = movieInfoGradient.bounds
-        
+        // Configure cast first so layouts are correct
         configureCast(topCast)
+        
+        let bgColor = view.backgroundColor ?? .white
+
+            synopsisGradient.setupGradient(
+                colors: [.clear, bgColor],
+                startPoint: CGPoint(x: 0.5, y: 0),
+                endPoint: CGPoint(x: 0.5, y: 1),
+                locations: [0.0, 0.3]
+            )
+
+            movieInfoGradient.setupGradient(
+                colors: [bgColor, .clear],
+                startPoint: CGPoint(x: 0.5, y: 1),
+                endPoint: CGPoint(x: 0.5, y: 0),
+                locations: [0.0, 1.0]
+            )
     }
 
+    private func setupNavigationBar() {
+        guard let navBar = navigationController?.navigationBar else { return }
+        navBar.setBackgroundImage(UIImage(), for: .default)
+        navBar.shadowImage = UIImage()
+        navBar.isTranslucent = true
+        navBar.backgroundColor = .clear
+        navBar.tintColor = .white
+    }
+
+    private func setupTabBar() {
+        guard let tabBar = tabBarController?.tabBar else { return }
+        tabBar.backgroundImage = UIImage()
+        tabBar.shadowImage = UIImage()
+        tabBar.isTranslucent = true
+        tabBar.backgroundColor = .clear
+    }
     
     @IBAction func expandedSynopsis(_ sender: UIButton) {
-      
         isExpanded.toggle()
-    
         synopsisContent.numberOfLines = isExpanded ? 0 : 3
         
         UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) {
             self.view.layoutIfNeeded()
         }
-
+        
         let imageName = isExpanded ? "arrow.up" : "arrow.down"
         expandButton.setImage(UIImage(systemName: imageName), for: .normal)
-
         synopsisGradient.isHidden = isExpanded
     }
-    
+
     func configureCast(_ cast: [CastMember]) {
         var index = 0
         
-        print("castStackView arrangedSubviews count:", castStackView.arrangedSubviews.count)
-        
-        for (hIndex, horizontalStack) in castStackView.arrangedSubviews.enumerated() {
-            guard let hStack = horizontalStack as? UIStackView else {
-                print("Subview \(hIndex) is not a UIStackView, skipping")
-                continue
-            }
-            print("Found horizontal stack \(hIndex), arrangedSubviews:", hStack.arrangedSubviews.count)
+        for horizontalStack in castStackView.arrangedSubviews {
+            guard let hStack = horizontalStack as? UIStackView else { continue }
             
-            for (vIndex, view) in hStack.arrangedSubviews.enumerated() {
+            for view in hStack.arrangedSubviews {
                 guard index < cast.count else { return }
                 let castMember = cast[index]
                 
@@ -150,23 +107,14 @@ class DetailsViewController: UIViewController {
                     imageView.image = castMember.image
                     imageView.layer.cornerRadius = imageView.bounds.width / 2
                     imageView.clipsToBounds = true
-                    print("Set image for horizontal stack \(hIndex), view \(vIndex)")
-                } else {
-                    print("No UIImageView with tag 1 in horizontal stack \(hIndex), view \(vIndex)")
                 }
                 
                 if let label = view.viewWithTag(2) as? UILabel {
                     label.text = castMember.name
-                    print("Set label text for horizontal stack \(hIndex), view \(vIndex)")
-                } else {
-                    print("No UILabel with tag 2 in horizontal stack \(hIndex), view \(vIndex)")
                 }
                 
                 index += 1
             }
         }
-        
-        print("Finished configuring cast")
     }
-
 }
